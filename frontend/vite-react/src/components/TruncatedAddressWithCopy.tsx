a501a11060a33f7eff9653720e6369f6bd6f7b83
Brage Hagemann Brataas <69582650+brahabra@users.noreply.github.com>
2025-03-31 16:13:55 +0200

    New design for account info modal (#67)
    New design for account info modal and minor tweaks to Transaction Confirmation Modal

--- frontend/vite-react/src/components/TruncatedAddressWithCopy.tsx
+++ frontend/vite-react/src/components/TruncatedAddressWithCopy.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import type React from 'react';
 import { Group, Tooltip, Text, CopyButton, ActionIcon } from '@mantine/core';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
-interface TruncatedCopyTextProps {
+interface TruncatedAddressWithCopyProps {
   inputString: string;
   startChars?: number;
   endChars?: number;
 }
 
-const TruncatedCopyText: React.FC<TruncatedCopyTextProps> = ({
+const TruncatedAddressWithCopy: React.FC<TruncatedAddressWithCopyProps> = ({
   inputString,
   startChars = 6,
   endChars = 5,
@@ -28,15 +28,15 @@ const TruncatedCopyText: React.FC<TruncatedCopyTextProps> = ({
       {({ copied, copy }) => (
         //Show full address on hover
         <Group>
-          <Tooltip label={inputString} withArrow>
-            <Text onClick={copy}>
+          <Tooltip w={540} label="This is address to your account. You can think of it as a bank account number for your digital assets. It's a unique public identifier." multiline>
+            <Text size='lg' onClick={copy}>
               {truncatedString}
             </Text>
           </Tooltip>
 
           <Tooltip label={copied ? "Copied" : "Copy"} withArrow>
             <ActionIcon color={copied ? "teal" : "gray"} variant="subtle" onClick={copy}> 
-              <ContentCopyIcon style={{ fontSize: 16 }} />
+              <ContentCopyIcon style={{ fontSize: 18 }} />
             </ActionIcon>
           </Tooltip>
         </Group>
@@ -45,4 +45,4 @@ const TruncatedCopyText: React.FC<TruncatedCopyTextProps> = ({
   );
 };
 
-export default TruncatedCopyText;
+export default TruncatedAddressWithCopy;
